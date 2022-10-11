c22a54edee63c96bca63713dfe42eec3cd0e4915
Joshua <joshuahsokolowski@gmail.com>
2022-10-11 04:32:42 -0400

    New upload

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # repostBot
-A discord bot to stop text reposts.
+
+## Description
+A joke bot for Discord designed to punish people by assigning a role to users who repost within a certain timeframe or by timing out users who mention a banned word.
+
+
+## Requirements
+Requires a .env file with a variable TOKEN assigned to an API key.
---
+++ repostBot.py
@@ -0,0 +1,232 @@
+from ctypes.wintypes import WORD
+from datetime import datetime
+import discord
+import os
+import sqlite3
+from dotenv import load_dotenv
+from datetime import timedelta
+
+# Function to clear old data from chat logs
+
+
+def clear(cursor, LENGTH):
+    cursor.execute(
+        "DELETE FROM chat WHERE (date < DATETIME('now', ?))", (LENGTH,))
+
+
+# Function to write message into database
+def writechat(conn, cursor, message):
+    # Put time, message content, and user into database, then commit
+    cursor.execute("INSERT INTO chat (date, messageDesc, memberID) VALUES (DATETIME('NOW', 'LOCALTIME'), ?, ?)",
+                   (message.content, message.author.id,))
+    conn.commit()
+
+
+# Function to write count of timeouts in database
+def writeban(conn, cursor, message):
+
+    # Otherwise, pull up their count, add 1 to it, and update it. Also commit it.
+    count = cursor.execute(
+        "SELECT count FROM members WHERE memberID = ?", (message.author.id,)).fetchone()[0]
+    count += 1
+    cursor.execute("UPDATE members SET count=? WHERE memberID=?",
+                   (count, message.author.id,))
+    conn.commit()
+
+    # We'll return the count for the announcement
+    return str(count)
+
+
+# Function to handle repost logic
+async def repost(conn, cursor, message, ROLE_ID):
+    # Assign content to variable, and then change it for images
+    mes = message.content
+
+    # Returns 0 if not a repost, 1 if a repost
+    real = cursor.execute("SELECT EXISTS(SELECT messageDesc FROM chat WHERE messageDesc = ?)", (mes,)).fetchone()
+
+    # If it's a repost, announce it to the channel
+    # remove the role from anyone who has it
+    # and give the reposter the role
+    # If it's not a repost, record post to database
+    if real[0] == 0:
+        writechat(conn, cursor, message)
+        return
+
+    if real[0] == 1:
+        # Get the whole row into a variable
+        post = cursor.execute(
+            "SELECT * FROM chat WHERE messageDesc = ?", (mes,)).fetchone()
+        time1, time2 = post[1].split()
+
+        # Announcement, using row information
+        await message.channel.send("Repost from " + message.author.mention + ".\n\n >>> On " + str(time1) + ", at " + str(time2) + ", <@" + str(post[3]) + "> posted: \n```" + post[2] + "```")
+
+        # Don't do role stuff if no role is set
+        if message.guild.get_role(ROLE_ID) == None:
+            await message.channel.send("No repost role set!")
+            return
+
+        # Go through users with role
+        role = discord.utils.get(message.guild.roles, id=ROLE_ID)
+
+        # Extra shame if the reposter already has the role
+        if role in message.author.roles:
+            await message.channel.send(message.author.mention + " already has the reposter role! They're a double reposter!")
+        else:
+            # Removal
+            try:
+                for member in message.guild.members:
+                    if role in member.roles:
+                        await member.remove_roles(role)
+
+                # Assignment
+                await message.author.add_roles(role)
+            except discord.errors.Forbidden:
+                await message.channel.send("Make sure my role is higher than the repost role! Otherwise, I can't assign or remove any roles!")
+
+    
+
+# Function to time out person for saying banned word
+
+
+async def wordban(conn, cursor, message, TIMEOUT_TIME):
+    # How long to time out the offender
+    time = timedelta(minutes=TIMEOUT_TIME)
+
+    # The bot can't time out an admin, so we're going to raise an exception if that happens
+    try:
+        # Otherwise, we're going to time out the offender
+        await message.author.timeout(time, reason="Timed out for saying a banned word!")
+    except discord.errors.Forbidden:
+        print("Admin unable to be timed out")
+
+    # Call a function to write to the database
+    count = writeban(conn, cursor, message)
+
+    # And inform the channel of the offense
+    await message.channel.send(message.author.mention + " has been timed out for saying a banned word.\nThey have said a banned word " + count + " times.")
+
+
+def main():
+
+    # Tell Discord the bot intents
+    intents = discord.Intents.default()
+    intents.messages = True
+    intents.guilds = True
+    intents.message_content = True
+    intents.members = True
+
+    # Pull info from .env file
+    load_dotenv()
+    TOKEN = os.getenv('TOKEN')
+
+    # Set bot as Discord client
+    client = discord.Client(intents=intents)
+
+    # Load up database; create database and tables if they doesn't exist.
+    try:
+        conn = sqlite3.connect('file:settings.sqlite?mode=rw', uri=True)
+        cursor = conn.cursor()
+    except sqlite3.OperationalError:
+        conn = sqlite3.connect('settings.sqlite')
+        cursor = conn.cursor()
+
+        cursor.execute("CREATE TABLE settings (guildID INTEGER PRIMARY KEY, roleID INTEGER, length TEXT DEFAULT '-2 days', wordban TEXT DEFAULT 'idiot', timeout INTEGER DEFAULT '2', ignoreChannels TEXT, ignoreWords TEXT DEFAULT 'hey hello hi yo ^ yeah ya yea lol no nah nope')")
+
+    # On launch, print to console
+
+    @client.event
+    async def on_ready():
+        print('We have logged in as {0.user}'.format(client))
+
+    @client.event
+    async def on_guild_join(joined):
+
+        # Create new settings row for guild in main database
+        cursor.execute(
+            "INSERT INTO settings (guildID) VALUES (?)", (joined.id,))
+        conn.commit()
+
+        try:
+            guildConn = sqlite3.connect(
+                f'file:{joined.id}.sqlite?mode=rw', uri=True)
+            guildCursor = guildConn.cursor()
+        except sqlite3.OperationalError:
+            guildConn = sqlite3.connect(f'{joined.id}.sqlite')
+            guildCursor = guildConn.cursor()
+
+            guildCursor.execute(
+                "CREATE TABLE members (memberID INTEGER PRIMARY KEY, count INTEGER DEFAULT '0')")
+
+            guildCursor.execute(
+                "CREATE TABLE chat (messageID INTEGER PRIMARY KEY, date DATE, messageDesc TEXT, memberID INTEGER, FOREIGN KEY(memberID) REFERENCES members(memberID))")
+
+            for member in joined.members:
+                guildCursor.execute(
+                    "INSERT INTO members (memberID) VALUES (?)", (member.id,))
+
+            guildConn.commit()
+
+    # On message,
+    @client.event
+    async def on_message(message):
+
+        # Ignore if this bot sent it
+        if message.author == client.user:
+            return
+
+        # Load options into variables
+        settings = (cursor.execute(
+            "SELECT * FROM settings WHERE guildID = ?", (message.guild.id,))).fetchall()[0]
+
+        ROLE_ID = settings[1]
+        DELETE_TIME = settings[2]
+        BANNED_WORDS = settings[3]
+        TIMEOUT_TIME = settings[4]
+        IGNORE_CHANNELS = str(settings[5])
+        EXEMPT_WORDS = settings[6]
+
+        if (message.content.startswith('$repost ')):
+            commands = message.content.split()
+            try:
+                if commands[1] == 'setrole':
+                    ROLE_ID = message.role_mentions[0].id
+                    cursor.execute("UPDATE settings SET roleID=? WHERE guildID=?", (ROLE_ID, message.guild.id,))
+                    await message.channel.send("Updated reposter role.")
+                    conn.commit()
+                    return
+            except IndexError:
+                return
+
+        # Only acknowledge whitelisted channel posts
+        if message.channel.id in IGNORE_CHANNELS.split():
+            return
+
+        # Ignore exempt phrases
+        for i in EXEMPT_WORDS.split():
+            if message.content == i:
+                return
+
+        # Connect to server's database
+        guildConn = sqlite3.connect(
+            f'file:{message.guild.id}.sqlite?mode=rw', uri=True)
+        guildCursor = guildConn.cursor()
+
+        # Whenever a message is sent, clear anything that's too old in the chat database.
+        clear(guildCursor, DELETE_TIME)
+
+        # Check for banned word
+        for i in BANNED_WORDS.split():
+            if i in message.content:
+                await wordban(guildConn, guildCursor, message, TIMEOUT_TIME)
+
+        # Check for reposts
+        if message.content != "":
+            await repost(guildConn, guildCursor, message, ROLE_ID)
+
+    # Run bot
+    client.run(TOKEN)
+
+
+main()
